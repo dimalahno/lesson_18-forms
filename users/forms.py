2052663778359392a054f3c46061b08db497a2e3
dimalahno <dimalahno@rambler.ru>
2025-04-01 16:33:37 +0500

    add login solution

--- users/forms.py
+++ users/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
+from django.core.exceptions import ValidationError
 from .models import CustomUser
 
 class RegistrationForm(UserCreationForm):
@@ -8,3 +9,10 @@ class RegistrationForm(UserCreationForm):
     class Meta:
         model = CustomUser
         fields = ["username", "phone_number", "gender", "email", "password1", "password2"]
+        
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+        if CustomUser.objects.filter(email=email).exists():
+            raise ValidationError("Этот адрес электронной почты уже зарегистрирован.")
+        return email
+            
